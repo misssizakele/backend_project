318e73d5013fdb7e17ddb3a250cdab6f28da25d5
Sizakele Mahlangu <misssizakele@gmail.com>
2023-06-09 20:20:03 +0200

    created an api to add a new record of a book

--- books/books/views.py
+++ books/books/views.py
@@ -1,8 +1,23 @@
 from django.http import JsonResponse
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+
+@api_view(['GET', 'POST'])
 def book_list(request):
-    books = Book.objects.all()      #get all the books
-    serializer = BookSerializer(books, many=True)      #serialize them
-    return JsonResponse({"books": serializer.data}, safe=False)        #return json
+    if request.method == 'GET':
+        books = Book.objects.all()      #get all the books
+        serializer = BookSerializer(books, many=True)      #serialize them
+        return JsonResponse({"books": serializer.data}, safe=False)        #return json
+
+    if request.method == 'POST':
+        #take the data you got
+        #de-serialize it
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
